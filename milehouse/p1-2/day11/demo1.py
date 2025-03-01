f7e69d4556aa2cd67e798ef0478357a713c45619
whong <weitian.hong@3ds.com>
2025-03-01 21:21:42 +0800

    learn python list

---
+++ milehouse/p1-2/day11/demo1.py
@@ -0,0 +1,4 @@
+list2=['张三',3.14,100,[1,2,3]]
+print(list2)
+print(len(list2))
+
---
+++ milehouse/p1-2/day12/demo1.py
@@ -0,0 +1,4 @@
+name_list=['张三','李四','王五']
+name_list.insert(0,'小明同学')
+name_list.append(123)
+print(len(name_list))
